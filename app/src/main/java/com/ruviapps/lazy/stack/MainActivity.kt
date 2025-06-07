c905386ba50e84dc43bee7cd2c2c1cd75402a748
vivekgupta <itguru4all@gmail.com>
2025-06-07 09:53:47 +0530

    Initial commit

---
+++ app/src/main/java/com/ruviapps/lazy/stack/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.ruviapps.lazy.stack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ruviapps.lazy.stack.ui.theme.LazyStackDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LazyStackDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
